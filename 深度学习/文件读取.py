745c7a35d963ea2030ca47c6ca1c869b226e30c3
wanghan0719 <jerry_dlut@163.com>
2020-12-29 17:02:41 +0800

    tensorflow文件读取

---
+++ 深度学习/文件读取.py
@@ -0,0 +1,43 @@
+# 利用读取器读取cvs文件
+import tensorflow as tf
+import os
+
+
+# tf.compat.v1.disable_eager_execution()
+
+# 定义读取函数：构建文件队列，定义读取器，读取内容，解码内容，批处理
+def csv_reader(filelist):  # filelist文件列表
+    # 构建文件队列
+    file_queue = tf.compat.v1.train.string_input_producer(filelist)
+    # 定义读取器，从文件队列中读取文件内容
+    reader = tf.compat.v1.TextLineReader()  # 文本读取器
+    k, v = reader.read(file_queue)  # 读取返回文件名及读到的数据
+    # 解码
+    records = [['None'], ['None']]  # 默认值
+    example, label = tf.compat.v1.decode_csv(v, record_defaults=records)
+    # 批处理，分批返回数据
+    example_bat, label_bat = tf.compat.v1.train.batch([example, label], batch_size=9, num_threads=1, capacity=9)
+    return example_bat, label_bat
+
+
+if __name__ == '__main__':
+    tf.compat.v1.disable_eager_execution()
+
+    filepath = 'data/test_data'
+    file_name = os.listdir(filepath)
+    file_list = [filepath + '/' + i for i in file_name]
+    print(file_list)
+
+    # 调用csv_read函数，构建队列，读取文件并返回
+    example, label = csv_reader(file_list)
+
+    with tf.compat.v1.Session() as sess:
+        coord = tf.compat.v1.train.Coordinator()  # 线程协调器，管理线程
+        # 创建一组线程
+        threads = tf.compat.v1.train.start_queue_runners(sess, coord=coord)
+        # 执行读取文件op
+        print(sess.run([example, label]))  # 执行这两个op
+
+        # 回收线程
+        coord.request_stop()
+        coord.join(threads=threads)
