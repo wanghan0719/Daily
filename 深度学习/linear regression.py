2e7715126d99724644f318cb5f99c8642ba53fab
Jerry <wh1039832283@163.com>
2021-01-07 16:55:35 +0800

    update

--- 深度学习/linear regression.py
+++ 深度学习/linear regression.py
@@ -2,37 +2,37 @@ import tensorflow as tf
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
-tf.compat.v1.disable_eager_execution()
+
 # 创建数据
-x = tf.compat.v1.random_normal([100, 1], mean=1.75, stddev=0.5, name='x_data')
+x = tf.random_normal([100, 1], mean=1.75, stddev=0.5, name='x_data')
 y_true = tf.matmul(x, [[2.0]]) + 5.0  # 矩阵相乘必须是二维的
 
 # 第二步：建立线性回归模型
 # 建立模型时，随机建立权重、偏置 y = wx + b
 # 权重需要不断更新，所以必须是变量类型. trainable指定该变量是否能随梯度下降一起变化
-weight = tf.Variable(tf.compat.v1.random_normal([1, 1]), name='w', trainable=True)  # 训练过程中值是否允许变化
+weight = tf.Variable(tf.random_normal([1, 1]), name='w', trainable=True)  # 训练过程中值是否允许变化
 bias = tf.Variable(0.0, name='b', trainable=True)  # 偏置
 y_pred = tf.matmul(x, weight) + bias  # 计算 wx + b
 
 # 第三步：求损失函数，误差(均方差)
-loss = tf.compat.v1.reduce_mean(tf.compat.v1.square(y_true - y_pred))
+loss = tf.reduce_mean(tf.square(y_true - y_pred))
 
 # # 第四步：使用梯度下降法优化损失
 # 学习率是比价敏感的参数，过小会导致收敛慢，过大可能导致梯度爆炸
-train_op = tf.compat.v1.train.GradientDescentOptimizer(0.1).minimize(loss)
+train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 # 收集损失值
-tf.compat.v1.summary.scalar('losses', loss)
-merged = tf.compat.v1.summary.merge_all()  # 将所有的摘要信息保存到磁盘，执行摘要合并
+tf.summary.scalar('losses', loss)
+merged = tf.summary.merge_all()  # 将所有的摘要信息保存到磁盘，执行摘要合并
 
-init_op = tf.compat.v1.global_variables_initializer()  # 初始化操作
+init_op = tf.global_variables_initializer()  # 初始化操作
 
-saver = tf.compat.v1.train.Saver()
-with tf.compat.v1.Session() as sess:  # 通过Session运行op
+saver = tf.train.Saver()
+with tf.Session() as sess:  # 通过Session运行op
     sess.run(init_op)  # 执行变量初始化
 
     # 指定事件文件
-    fw = tf.compat.v1.summary.FileWriter("summary", graph=sess.graph)  # 定义文件写入器
+    fw = tf.summary.FileWriter("summary", graph=sess.graph)  # 定义文件写入器
 
     # 判断模型文件是否存在，存在则加载，不存在则创建
     if os.path.exists('model/linear_model/checkpoint'):  # 模型已存在
--- 深度学习/图片数据读取.py
+++ 深度学习/图片数据读取.py
@@ -2,26 +2,26 @@ import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
+
 
 # 定义图片读取函数
 def img_reader(filelist):  # filelist文件列表
     # 构建文件队列
-    file_queue = tf.compat.v1.train.string_input_producer(filelist)
+    file_queue = tf.train.string_input_producer(filelist)
     # 定义读取器，从文件队列中读取文件内容
-    reader = tf.compat.v1.WholeFileReader()  # 文本读取器
+    reader = tf.WholeFileReader()  # 文本读取器
     k, v = reader.read(file_queue)  # 读取返回文件名及读到的数据
     # 解码
-    img = tf.compat.v1.image.decode_jpeg(v)
-    img_resize = tf.compat.v1.image.resize(img, size=[200, 200])
+    img = tf.image.decode_jpeg(v)
+    img_resize = tf.image.resize_images(img, size=[200, 200])
     img_resize.set_shape([200, 200, 3])  # 固定样本形状，批处理时对形状有要求，修改张量的静态形状
     # 批处理，分批返回数据
-    img_bat = tf.compat.v1.train.batch([img_resize], batch_size=10, num_threads=1)
+    img_bat = tf.train.batch([img_resize], batch_size=10, num_threads=1)
     return img_bat
 
 
 if __name__ == '__main__':
-    tf.compat.v1.disable_eager_execution()
-
     filepath = 'data/test_img'
     file_name = os.listdir(filepath)
     file_list = [filepath + '/' + i for i in file_name]
@@ -30,10 +30,10 @@ if __name__ == '__main__':
     # 调用csv_read函数，构建队列，读取文件并返回
     img = img_reader(file_list)
 
-    with tf.compat.v1.Session() as sess:
-        coord = tf.compat.v1.train.Coordinator()  # 线程协调器，管理线程
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()  # 线程协调器，管理线程
         # 创建一组线程
-        threads = tf.compat.v1.train.start_queue_runners(sess, coord=coord)
+        threads = tf.train.start_queue_runners(sess, coord=coord)
         # 执行读取文件op
         # print(sess.run(img))  # 执行这两个op
         imgs = img.eval()
