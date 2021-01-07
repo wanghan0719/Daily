189f709de482f00e5de6e51f28d768e3ea4d8a3e
Jerry <wh1039832283@163.com>
2021-01-07 17:02:17 +0800

    update

--- 深度学习/tensorflow基础.py
+++ 深度学习/tensorflow基础.py
@@ -3,11 +3,10 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 
-# tf.compat.v1.disable_eager_execution()
 # hello是一个张量（常量），也是操作（数据计算规则），还没有执行
 # hello = tf.constant('hello world')
 # print(hello)
-# sess = tf.compat.v1.Session()  # 创建session对象
+# sess = tf.Session()  # 创建session对象
 # print(sess.run(hello))  # 执行hello操作
 # sess.close()  # 关闭session
 
@@ -18,7 +17,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 # print(c)
 
 # 获取默认graph
-# graph = tf.compat.v1.get_default_graph()  # 获取缺省图
+# graph = tf.get_default_graph()  # 获取缺省图
 # print(graph)
 
 # 创建新graph
@@ -27,7 +26,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 # with graph2.as_default():  # 设置默认graph
 #     d = tf.constant(11.0)
 
-# with tf.compat.v1.Session(graph=graph2) as sess:
+# with tf.Session(graph=graph2) as sess:
 #     print(sess.run(d))
 #     print(sess.graph)
 #     print(d.shape)
@@ -44,11 +43,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 # 生成值全为1的张量
 # tensor_ones = tf.ones(shape=[2, 3], dtype="float32")
 # 创建正态分布张量
-# tensor_nd = tf.compat.v1.random_normal(shape=[10], mean=1.7, stddev=0.2, dtype="float32")
+# tensor_nd = tf.random_normal(shape=[10], mean=1.7, stddev=0.2, dtype="float32")
 # 生成和输入张量形状一样的张量，值全为1
 # tensor_zeros_like = tf.zeros_like(tensor_ones)
 
-# with tf.compat.v1.Session() as sess:
+# with tf.Session() as sess:
 #     print(tensor_zeros.eval())  # eval表示在session中计算该张量
 #     print(tensor_ones.eval())
 #     print(tensor_nd.eval())
@@ -59,10 +58,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 占位符
 """
 # 不确定数据，先使用占位符占个位置
-# plhd = tf.compat.v1.placeholder(tf.float32, [2, 3])  # 2行3列的tensor
-# plhd2 = tf.compat.v1.placeholder(tf.float32, [None, 3])  # N行3列的tensor
+# plhd = tf.placeholder(tf.float32, [2, 3])  # 2行3列的tensor
+# plhd2 = tf.placeholder(tf.float32, [None, 3])  # N行3列的tensor
 
-# with tf.compat.v1.Session() as sess:
+# with tf.Session() as sess:
 #     d = [[1, 2, 3],
 #          [4, 5, 6]]
 #     print(sess.run(plhd, feed_dict={plhd: d}))
@@ -75,7 +74,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 """
 张量形状改变
 """
-# pld = tf.compat.v1.placeholder(tf.float32, [None, 3])
+# pld = tf.placeholder(tf.float32, [None, 3])
 # print(pld)
 
 # pld.set_shape([4, 3])
@@ -95,9 +94,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 # # 创建变量
 # var = tf.Variable(initial_value=tf.random.normal([2,3]), name='variable')
 # # 初始化变量
-# init_op=tf.compat.v1.global_variables_initializer()
+# init_op=tf.global_variables_initializer()
 #
-# with tf.compat.v1.Session() as sess:
+# with tf.Session() as sess:
 #     sess.run(init_op)
 #     print(sess.run([a, var])) #执行多个op
 
@@ -112,7 +111,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 调整警告级别
 # 创建普通张量
 a = tf.constant([1, 2, 3, 4, 5])
 # 创建变量
-var = tf.Variable(tf.compat.v1.random_normal([2, 3], mean=0.0, stddev=1.0),
+var = tf.Variable(tf.random_normal([2, 3], mean=0.0, stddev=1.0),
                   name="variable")
 
 b = tf.constant(3.0, name="a")
@@ -120,12 +119,12 @@ c = tf.constant(4.0, name="b")
 d = tf.add(b, c, name="add")
 
 # 变量必须显式初始化, 这里定义的是初始化操作，并没有运行
-init_op = tf.compat.v1.global_variables_initializer()
+init_op = tf.global_variables_initializer()
 
 # with tf.compat.v1.Session() as sess:
 #     sess.run(init_op)
 #     # 将程序图结构写入事件文件
-fw = tf.summary.create_file_writer("summary")
+fw = tf.summary.FileWriter("summary")
 fw.flush()
 # print(sess.run([a, var]))
 
--- 深度学习/文件读取.py
+++ 深度学习/文件读取.py
@@ -8,21 +8,19 @@ import os
 # 定义读取函数：构建文件队列，定义读取器，读取内容，解码内容，批处理
 def csv_reader(filelist):  # filelist文件列表
     # 构建文件队列
-    file_queue = tf.compat.v1.train.string_input_producer(filelist)
+    file_queue = tf.train.string_input_producer(filelist)
     # 定义读取器，从文件队列中读取文件内容
-    reader = tf.compat.v1.TextLineReader()  # 文本读取器
+    reader = tf.TextLineReader()  # 文本读取器
     k, v = reader.read(file_queue)  # 读取返回文件名及读到的数据
     # 解码
     records = [['None'], ['None']]  # 默认值
-    example, label = tf.compat.v1.decode_csv(v, record_defaults=records)
+    example, label = tf.decode_csv(v, record_defaults=records)
     # 批处理，分批返回数据
-    example_bat, label_bat = tf.compat.v1.train.batch([example, label], batch_size=9, num_threads=1, capacity=9)
+    example_bat, label_bat = tf.train.batch([example, label], batch_size=9, num_threads=1, capacity=9)
     return example_bat, label_bat
 
 
 if __name__ == '__main__':
-    tf.compat.v1.disable_eager_execution()
-
     filepath = 'data/test_data'
     file_name = os.listdir(filepath)
     file_list = [filepath + '/' + i for i in file_name]
@@ -31,10 +29,10 @@ if __name__ == '__main__':
     # 调用csv_read函数，构建队列，读取文件并返回
     example, label = csv_reader(file_list)
 
-    with tf.compat.v1.Session() as sess:
-        coord = tf.compat.v1.train.Coordinator()  # 线程协调器，管理线程
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()  # 线程协调器，管理线程
         # 创建一组线程
-        threads = tf.compat.v1.train.start_queue_runners(sess, coord=coord)
+        threads = tf.train.start_queue_runners(sess, coord=coord)
         # 执行读取文件op
         print(sess.run([example, label]))  # 执行这两个op
 
