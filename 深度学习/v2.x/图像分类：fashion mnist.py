06c8020a0a409ec3dc30371d9a3b8002dfb18d34
wanghan0719 <jerry_dlut@163.com>
2021-01-08 21:56:41 +0800

    tensorflow2.0-fashion mnist分类

---
+++ 深度学习/v2.x/图像分类：fashion mnist.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 加载数据
+fashion_mnist = keras.datasets.fashion_mnist
+(train_img, train_labels), (test_img, test_labels) = fashion_mnist.load_data()  # 读取训练及测试数据
+class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+              'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']  # 标签名
+# 查看数据
+# print(train_img.shape,len(train_labels))
+# print(train_labels)
+# plt.figure()
+# plt.imshow(test_img[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# 将这些值缩小至0到1之间，然后将其馈送到神经网络模型
+train_img = train_img / 255
+test_img = test_img / 255
+plt.figure()
+plt.imshow(test_img[0], cmap='binary')
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+# 构建模型
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # 将图像格式从二维数组（28 x 28 像素）转换成一维数组（28 x 28 = 784 像素）
+    keras.layers.Dense(units=128, activation='relu'),  # 第二层全连接层
+    keras.layers.Dense(units=10)
+])
+
+# 编译模型
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(train_img, train_labels, epochs=10)
+
+# 评估模型
+test_loss, test_acc = model.evaluate(test_img, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_img)
+print(class_name[tf.argmax(predictions[0])])
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap='binary')
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_name[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_name[true_label]), color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# 验证预测结果
+i = 0
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_img)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_img)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions[i], test_labels, test_img)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
